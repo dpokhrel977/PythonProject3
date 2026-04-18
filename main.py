c705f10ddaffa801f79f674e0aa487fbcd4f49ab
dpokhrel <dpokhrel@reisystems.com>
2026-04-17 20:40:45 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Hi, {name}")
+    print(os.environ.get("SERVER.PORT"))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    print_hi("PyCharm")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
